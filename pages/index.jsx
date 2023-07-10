f6829d0b95dcebfdb935d79cd80af17a1232f519
ravenakin <139095292+ravenakin@users.noreply.github.com>
2023-07-10 23:38:04 +0900

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,45 @@
 import styles from "../styles/Home.module.css";
-import InstructionsComponent from "../components/InstructionsComponent";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <nav>
+          <ul className={styles.navList}>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
       <main className={styles.main}>
-        <InstructionsComponent></InstructionsComponent>
+        <section id="home" className={styles.home}>
+          <h1>Welcome to our Web3 World</h1>
+          <p>Experience the future of the web with blockchain technology.</p>
+        </section>
+        <section id="about" className={styles.about}>
+          <h2>About Us</h2>
+          <p>We are pioneers in integrating blockchain with web development.</p>
+        </section>
+        <section id="services" className={styles.services}>
+          <h2>Services</h2>
+          <p>We offer various services related to Web3 development.</p>
+        </section>
+        <section id="contact" className={styles.contact}>
+          <h2>Contact Us</h2>
+          <form>
+            <input type="text" placeholder="Your name" required />
+            <input type="email" placeholder="Your email" required />
+            <textarea placeholder="Your message"></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </section>
+        <InstructionsComponent/>
       </main>
+      <footer className={styles.footer}>
+        <p>© 2023 Our Web3 World. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
